e950b1708ab4decf1b91a3327af93fd9c5fb0495
Matteo <matteoamodio17@gmail.com>
2024-03-08 16:33:57 +0100

    file updated

---
+++ lonfo.js
@@ -0,0 +1,35 @@
+function lonfo(text) {
+    const noPunctuation = text.replace(/[^\w\s]/g, '').toLowerCase();
+  
+    const words = [...new Set(noPunctuation.split(/\s+/))];
+  
+    const sortedWords = words.sort();
+  
+    const result = sortedWords.join(' ');
+  
+    return result;
+  }
+  
+  const text = `
+  Il lonfo non vaterca né gluisce
+  e molto raramente barigatta,
+  ma quando soffia il bego a bisce bisce
+  sdilenca un poco, e gnagio s’archipatta.
+  
+  È frusco il lonfo! È pieno di lupigna
+  arrafferia malversa e sofolenta!
+  Se cionfi ti sbiduglia e t’arrupigna
+  se lugri ti botalla e ti criventa.
+  
+  Eppure il vecchio lonfo ammargelluto
+  che bete e zugghia e fonca nei trombazzi
+  fa lègica busìa, fa gisbuto;
+  
+  e quasi quasi in segno di sberdazzi
+  gli affarferesti un gniffo. Ma lui zuto
+  t’alloppa, ti sbernecchia; e tu l’accazzi.
+  `;
+  
+  const result = lonfo(text);
+  console.log(result);
+  
