969d18c695f38ecf0dd737181e7fc6b1b0ccba92
Matteo <matteoamodio17@gmail.com>
2024-03-11 11:08:16 +0100

    Updated exercise

--- lonfo.js
+++ lonfo.js
@@ -1,3 +1,26 @@
+const text = `
+Il lonfo non vaterca né gluisce
+e molto raramente barigatta,
+ma quando soffia il bego a bisce bisce
+sdilenca un poco, e gnagio s’archipatta.
+
+È frusco il lonfo! È pieno di lupigna
+arrafferia malversa e sofolenta!
+Se cionfi ti sbiduglia e t’arrupigna
+se lugri ti botalla e ti criventa.
+
+Eppure il vecchio lonfo ammargelluto
+che bete e zugghia e fonca nei trombazzi
+fa lègica busìa, fa gisbuto;
+
+e quasi quasi in segno di sberdazzi
+gli affarferesti un gniffo. Ma lui zuto
+t’alloppa, ti sbernecchia; e tu l’accazzi.
+`;
+
+const result = lonfo(text);
+console.log(result);
+
 function lonfo(text) {
     const noPunctuation = text.replace(/[^\w\s]/g, '').toLowerCase();
   
@@ -8,28 +31,4 @@ function lonfo(text) {
     const result = sortedWords.join(' ');
   
     return result;
-  }
-  
-  const text = `
-  Il lonfo non vaterca né gluisce
-  e molto raramente barigatta,
-  ma quando soffia il bego a bisce bisce
-  sdilenca un poco, e gnagio s’archipatta.
-  
-  È frusco il lonfo! È pieno di lupigna
-  arrafferia malversa e sofolenta!
-  Se cionfi ti sbiduglia e t’arrupigna
-  se lugri ti botalla e ti criventa.
-  
-  Eppure il vecchio lonfo ammargelluto
-  che bete e zugghia e fonca nei trombazzi
-  fa lègica busìa, fa gisbuto;
-  
-  e quasi quasi in segno di sberdazzi
-  gli affarferesti un gniffo. Ma lui zuto
-  t’alloppa, ti sbernecchia; e tu l’accazzi.
-  `;
-  
-  const result = lonfo(text);
-  console.log(result);
-  
+}
